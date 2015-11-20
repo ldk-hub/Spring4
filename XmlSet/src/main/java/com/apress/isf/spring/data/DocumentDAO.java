c670a333960c8ab793e224a3d4c9790098dc69d5
ok <ok@ok-PC>
2015-11-20 11:47:18 +0900

    chapter3

---
+++ XmlSet/src/main/java/com/apress/isf/spring/data/DocumentDAO.java
@@ -0,0 +1,8 @@
+package com.apress.isf.spring.data;
+
+import com.apress.isf.java.model.Document;
+
+public interface DocumentDAO {
+
+	public Document[] getAll();//DocumentRepository의 데이터타입및 데이터를 배열로담는다.
+}
