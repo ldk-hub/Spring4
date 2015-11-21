806f370de313407c80513aa8fb48259d86cc9edf
ok <ok@ok-PC>
2015-11-21 18:45:14 +0900

    chapter5 (리스트,맵 프로퍼티)

---
+++ CustomType/src/main/java/com/apress/isf/spring/data/TypeDataDAO.java
@@ -0,0 +1,9 @@
+package com.apress.isf.spring.data;
+
+import com.apress.isf.java.model.Type;
+
+public interface TypeDataDAO {
+	public Type[] getAll();
+	//지정한 id를 기반으로 Type을 얻게 한다.
+	public Type findById(String id);
+}
